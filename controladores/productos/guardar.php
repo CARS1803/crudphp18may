86eac45396fb5b01b22c9d363e43ffc494918d41
Reyes <Carlosreyes1803@gmail.com>
2023-05-18 13:36:52 -0600

    carpeta acutualizda donde la dejo mi alferez.

---
+++ controladores/productos/guardar.php
@@ -0,0 +1,11 @@
+<?php
+
+require '../../modelos/Producto.php';
+
+try {
+    $producto = new Producto($_POST);
+    $resultado = $producto->guardar();
+    var_dump($resultado);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -1,4 +1,5 @@
 <?php
+
 abstract class Conexion{
     public static $conexion = null;
 
@@ -29,5 +30,20 @@ abstract class Conexion{
         self::$conexion = null;
         // DEVOLVEMOS RESULTADOS
         return $resultado;
+    }
+
+    public static function servir($sql){
+        // CONECTANDOSE A LA BD CON EL METODO CONECTAR
+        self::conectar();
+        // PREPARAMOS LA SENTENCIA
+        $sentencia = self::$conexion->prepare($sql);
+        // EJECUTAMOS A SENTENCIA
+        $sentencia->execute();
+        $resultados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+        // CERRANDO LA CONEXION
+        self::$conexion = null;
+        // DEVOLVEMOS RESULTADOS
+        return $resultados;
     }
 }
